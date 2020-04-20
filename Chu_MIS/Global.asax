e2ec900643f79b25867816d28774cc611f126d2f
yckoinchu <eugene@g.chu.edu.tw>
2020-04-20 17:21:09 +0800

    first commit

---
+++ Chu_MIS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Chu_MIS.MvcApplication" Language="C#" %>
---
+++ Chu_MIS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Chu_MIS.Startup))]
+namespace Chu_MIS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
