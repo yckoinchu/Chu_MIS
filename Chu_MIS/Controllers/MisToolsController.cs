8c066e006dd0195dda2676098b7a958246073f04
yckoinchu <eugene@g.chu.edu.tw>
2020-04-21 16:30:29 +0800

    add MisToolsController

---
+++ Chu_MIS/Controllers/MisToolsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Chu_MIS.Controllers
+{
+    public class MisToolsController : Controller
+    {
+        // GET: MisTools
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
